c12f73745b48fb7597954e1212f185e6acdd030b
zhighman <36615938+zhighman@users.noreply.github.com>
2018-02-19 13:15:06 -0500

    Update plex_reboot.sh

--- plex_reboot.sh
+++ plex_reboot.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
-
+  
 #pull the latest status file from git
-status=/tmp/plex_status.dat
-curl -o $status https://raw.githubusercontent.com/zhighman/plex_status/master/status.dat
-
-
+status=/Users/zachary/Documents/plex_status/status.dat
+git fetach --all
+git reset --hard origin/master
 
 grep -e "reboot" $status
 RC=$?
 
 if [[ $RC -eq 0 ]]; then
-	killAll "Plex Media Server"
-	open /Applications/Plex\ Media\ Server.app
+        killAll "Plex Media Server"
+        open /Applications/Plex\ Media\ Server.app
 
-	now=`date`
-	echo $now > ~/Documents/plex_status/status.dat
-	cd /Documents/git/plex_status/
-	git commit -m "rebooted PMS"
-	git push -u origin master
+        now=`date`
+        echo $now > $status
+        git commit -m "rebooted PMS"
+        git push -u origin master
 fi
 
 exit 0
